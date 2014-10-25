cc69fa59622f3c9d8b6df58ae8b291e22babaf9d
SValeriu <statache.valeriu@gmail.com>
2014-10-25 18:15:45 +0300

    modified gitignore, added main class

---
+++ src/main.java
@@ -0,0 +1,11 @@
+
+/* 
+ * author: Valeriu Statache
+ * All rights reserved 2014
+ */
+public class main {
+	public static void main(String [] args) {
+		// Prints to stdout greeting message
+		System.out.println("Hello wonderful world!");
+	}
+}
